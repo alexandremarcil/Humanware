9eafe37a5de14be4ac588520681a0582469e5c8d
Zigzagzen7 <alexandre.marcil@gmail.com>
2019-03-15 15:51:10 -0400

    code cleanup

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -8,7 +8,6 @@ import torch
 from tqdm import tqdm
 
 from utils.config import cfg
-from tensorboardX import SummaryWriter
 
 
 def train_model(model, train_loader, valid_loader, device, writer,
@@ -131,20 +130,18 @@ def train_model(model, train_loader, valid_loader, device, writer,
 
                 predicted_digits = torch.cat(
                    [torch.max(output_digit.data, dim=1)[1].unsqueeze(1)
-                     for output_digit in outputs_digits],
-                    dim=1)
+                    for output_digit in outputs_digits], dim=1)
 
                 # Calculate the accuracy
                 good_digits = predicted_digits == target_digits
                 good_ndigit = predicted_ndigits == target_ndigits
 
-                
                 # Verify that for each digits of a sequence
                 # the digit is not in the image (-1) 
                 # or the digit is in the image (!= -1) and it is predict correctly.
                 # Check if this is true for all digits in the sequence (torch.prod dim=1).
                 all_good_digits = torch.prod(((target_digits == -1)
-                                              + (target_digits != -1) * good_digits),dim=1, dtype=torch.uint8)
+                                              + (target_digits != -1) * good_digits), dim=1, dtype=torch.uint8)
 
                 good_sequence = all_good_digits * good_ndigit
 
@@ -180,7 +177,7 @@ def train_model(model, train_loader, valid_loader, device, writer,
                 'digit5': digits_acc[5]}, epoch + 1)
 
             if phase == "valid":
-                #Keep a copy of the best model
+                # Keep a copy of the best model
                 if sequence_acc > valid_best_accuracy:
                     valid_best_accuracy = sequence_acc
                     best_model = copy.deepcopy(model)
